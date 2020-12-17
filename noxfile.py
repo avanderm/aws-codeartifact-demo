5fe079ba06a8a21c2c848f8c1c9735054deb032a
Antoine Vandermeersch <avdmeers@gmail.com>
2020-12-17 23:13:32 +0100

    test stage fixes

--- noxfile.py
+++ noxfile.py
@@ -5,13 +5,13 @@ nox.options.sessions = "lint", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.8", "3.7"])
+@nox.session(python=["3.7", "3.8"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
 
 
-@nox.session(python=["3.8", "3.7"])
+@nox.session(python="3.8")
 def lint(session):
     args = session.posargs or locations
     session.install("flake8", "flake8-black")
